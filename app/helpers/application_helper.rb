5cf887b73439075a377a53948341f2d0863ab6d2
tuannguyen <nguyen.quang.tuan@sun-asterisk.com>
2019-04-07 11:26:18 +0700

    Commit on weekend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title page_title = ''
+    base_title = t(".Ruby_on_Rails_Tutorial_Sample_App")
     if page_title.empty?
       base_title
     else
