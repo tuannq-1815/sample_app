9362fa3b256fd449a8f51d10cbd268fd79410935
tuannguyen <nguyen.quang.tuan@sun-asterisk.com>
2019-04-08 09:54:58 +0700

    Fix cmt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title page_title = ''
+  def full_title page_title = ""
     base_title = t(".Ruby_on_Rails_Tutorial_Sample_App")
     if page_title.empty?
       base_title
